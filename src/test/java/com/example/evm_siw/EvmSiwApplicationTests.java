c27e257842da2ebf38c028b2551ef2ad33f237e8
carlo-da <flyitnavy@gmail.com>
2025-12-27 09:11:43 +0100

    Initial commit

---
+++ src/test/java/com/example/evm_siw/EvmSiwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evm_siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvmSiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
